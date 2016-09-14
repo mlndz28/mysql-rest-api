a4624b4274ac20a53fa9f82ab57814927c0e9645
mlndz <ie0217@iriria.eie.ucr.ac.cr>
2016-09-14 11:44:16 -0600

    modularization v0.1

--- pool.js
+++ pool.js
@@ -5,11 +5,11 @@ var port = 2828;
 
 var pool = mysql.createPool({ //connection to db
 	connectionLimit: 100,
-	host: "10.0.0.23",
+	host: "localhost",
 	port: "3306",
-	user: "root",
-	password: "2347771",
-	database: "modelador_flujos"
+	user: "mlndz",
+	password: "colacho",
+	database: "newTest"
 });
 
 console.log("Initializing server...")
@@ -21,14 +21,14 @@ console.log("Getting connection to DB...")
 
 var router = express.Router(); //new module for 'app'
 router.get("/", function (req, res) {
-	getUsers(pool, res);
+	djent(pool);
 });
 app.use("/new", router); //add router to app
 app.listen(port);
 
 //connection.end();
 
-function getUsers(pool, res) {
+/*function getUsers(pool, res) {
 	console.log("yes");
 	var rowsResponse = new Object();
 
@@ -40,7 +40,7 @@ function getUsers(pool, res) {
 
 		console.log("User: ", connection.threadId);
 
-		connection.query("SELECT * from MfF_Usuarios", function (err, rows, fields) {
+		connection.query("SELECT * from djent", function (err, rows, fields) {
 			if (!err) {
 				rowsResponse["error"] = "none";
 				rowsResponse["data"] = rows;
@@ -53,4 +53,34 @@ function getUsers(pool, res) {
 	});
 	//console.log(rowsResponse);
 	//return rowsResponse;
+}*/
+
+function djent(pool) {
+	
+	getConnection(pool).query("SELECT * from djent", function (err, rows, fields) {
+		if (!err) {
+			rowsResponse["error"] = "none";
+			rowsResponse["data"] = rows;
+		} else {
+			rowsResponse["error"] = err;
+			console.log("error");
+		}
+		res.json(rowsResponse);
+	});
+}
+
+function getConnection(pool) {
+	//var _connection;
+
+	return pool.getConnection(function (err, connection) {
+		if (err) {
+			connection.release();
+			res.json = new Object()["error"] = err;
+		}
+
+		console.log("User: ", connection.threadId);
+		return connection;
+	});
+
+	//return _connection;
 }
