2935e3749c063a23f295dfea2f71c6ee61a7d0c0
mlndz <ie0217@iriria.eie.ucr.ac.cr>
2016-09-17 15:11:30 -0600

    connection.js

--- api/connection.js
+++ api/connection.js
@@ -4,48 +4,62 @@ var conf = require("../conf/default.json").mysql;
 var pool;
 
 exports.createPool = function () {
-	this.pool = mysql.createPool(conf); //create new connection pool
-
-	this.pool.config.connectionConfig.queryFormat = function (query, values) { //so we can use prepared statements with the format :label instead of ?
-		console.log("query0 = " + query);
-		if (!values) return query;
-		var temp = query.replace(/\:(\w+)/g, function (txt, key) {
-			if (values.hasOwnProperty(key)) {
-				return this.escape(values[key]);
-			} else {
-				return '';
-			}
-			return txt;
-		}.bind(this));
-		console.log("query1 = " + temp);
-		return temp;
-	};
+    this.pool = mysql.createPool(conf); //create new connection pool
+    this.pool.config.connectionConfig.queryFormat = parseQuery; //adds formatting to prepared statements
 }
 
 exports.query = function (statement, body, res) { //@param statement: MySQL query;@param body: object with input data
-	var resObject = new Object();
 
-	this.pool.getConnection(function (err, connection) {
-		if (err) { //if can't connect to DB
-			resObject["error"] = err;
-			console.error("out =" + err);
-			res.json(resObject);
-		} else {
+    this.pool.getConnection(function (err, connection) {
+        if (err) { //if can't connect to DB
+            onError(err, res);
+        } else {
+            onConnect(statement, body, connection, res);
+        }
+    });
+}
 
-			connection.query(statement, body, function (err, results) {
-				console.log("in = " + JSON.stringify(body));
-				connection.release(); //as it's not being used anymore
-				if (!err) {
-					resObject["error"] = "none";
-					resObject["data"] = results;
-					console.log("out = " + JSON.stringify(resObject));
-				} else { //if query can't be executed
-					resObject["error"] = err;
-					console.error("out = " + err);
-				}
-				res.json(resObject);
-				return resObject;
-			});
-		}
-	});
+/*
+ *   New format for prepared statements, :label is the new standard.
+ *   This way objects can be used as parameters for querys.
+ */
+
+function parseQuery(statement, values) {
+    //console.log("query0 = " + statement);
+    if (!values) return statement;
+    var temp = statement.replace(/\:(\w+)/g, function (txt, key) {
+        if (values.hasOwnProperty(key)) {
+            return this.escape(values[key]);
+        } else {
+            return '';
+        }
+        return txt;
+    }.bind(this));
+    //console.log("query1 = " + temp);
+    return temp;
+
+}
+
+function onConnect(statement, body, connection, res) {
+    connection.query(statement, body, function (err, results) {
+        console.log("in = " + JSON.stringify(body));
+        connection.release(); //as it's not being used anymore
+        if (!err) {
+            var resObject = new Object();
+            resObject["error"] = "none";
+            resObject["data"] = results;
+            console.log("out = " + JSON.stringify(resObject));
+            res.json(resObject);
+        } else { //if query can't be executed
+            onError(err, res);
+        }
+        return resObject;
+    });
+}
+
+function onError(err, res) {
+    console.error("out =" + err);
+    res.json({
+        error: err
+    });
 }
---
+++ conf/23.js
@@ -0,0 +1,16 @@
+{
+	"mysql": {
+		"connectionLimit": 101,
+		"host": "10.0.0.23",
+		"port": "3306",
+		"user": "root",
+		"password": "2347771",
+		"database": "modelador_flujos"
+
+	},
+
+	"express": {
+		"port": 2828
+	}
+
+}
