3b3999a71feee0e47ddd0e85ee450c1fa4df4758
Fabian Melendez <mlndzodin@gmail.com>
2017-10-13 19:55:35 -0400

    Express exceptions handled.
    Listener added for uncaught exceptions (the only way Express exceptions can be handled without modifying Express objects).

--- api/app.js
+++ api/app.js
@@ -1,6 +1,7 @@
 var express = require("express");
+var cli = require("cli");
 
-var conf = require("../conf/default.json").express;
+var conf = require("../server").configuration.express;
 
 /**
  * Start the API
@@ -8,19 +9,34 @@ var conf = require("../conf/default.json").express;
  */
 function app() {
 	var app = express(); //start API
-	console.info("Server running on port " + conf.port + ".");
-	app.use(function (req, res, next) {
+
+	process.on('uncaughtException', function(err) {
+		if (err) {
+			switch (err.code) {
+				case "EADDRINUSE":
+					cli.error("The port is already taken.");
+					break;
+			}
+		}
+		process.exit();
+	});
+
+
+	app.use(function(req, res, next) {
+		console.log("[" + req.method + "]"+ (req.method.length > 5 ? "\t":"\t\t") + req.url);
 		res.header("Access-Control-Allow-Origin", "*");
 		res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 		next();
 	});
 
 
-	var bodyParser = require('body-parser') //in order to get body content from requests
+	var bodyParser = require('body-parser'); //in order to get body content from requests
 	app.use(bodyParser.urlencoded({
 		extended: true
 	}));
 	app.listen(conf.port); //port set on conf file
+
+	console.info("Server running on port " + conf.port + ".");
 	return app;
 }
 
